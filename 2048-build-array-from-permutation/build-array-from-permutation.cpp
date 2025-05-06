24f2326145d3de731ff995f547112fb75208992b
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-05-06 20:32:56 +0300

    Time: 0 ms (100.00%) | Memory: 20.3 MB (81.05%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    // github code
+    vector<int> buildArray(vector<int>& a) {
+        vector<int> ans(a.size());
+        for (int i = 0; i < a.size(); i++)
+            ans[i] = a[a[i]];
+        return ans;
+    }
+};
