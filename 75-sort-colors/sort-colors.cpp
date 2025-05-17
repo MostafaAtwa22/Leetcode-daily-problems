4c6f566ec42e2d63cdd1e110fac9fc63b1efcb49
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-05-17 05:01:15 +0300

    Time: 0 ms (100.00%) | Memory: 11.6 MB (48.91%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void sortColors(vector<int>& a) {
+        int n = a.size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (a[j] > a[j + 1])
+                    swap(a[j], a[j + 1]);
+            }
+        }
+    }
+};
