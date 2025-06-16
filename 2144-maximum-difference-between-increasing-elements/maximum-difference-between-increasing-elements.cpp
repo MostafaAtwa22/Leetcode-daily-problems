ba3aa5daef3404d2921f4f7869c77bbb630df15a
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-06-16 12:09:15 +0300

    Time: 0 ms (100.00%) | Memory: 12.6 MB (5.67%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -3,12 +3,18 @@ public:
     int maximumDifference(vector<int>& a) {
         int n = a.size();
         int ans = INT_MIN;
-        for (int i = 0; i < n; i++)
+        vector<int> mx(n + 1, 0);
+        int maxx = a[n - 1];
+        int minn = a[0];
+        for (int i = n - 1; i >= 0; i--)
         {
-            for (int j = i + 1; j < n; j++)
-            {
-                ans = max(a[j] - a[i], ans);
-            }
+            maxx = max(maxx, a[i]);
+            mx[i] = maxx;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            minn = min(minn, a[i - 1]);
+            ans = max(mx[i - 1] - minn, ans);
         }
         return ans <= 0 ? -1 : ans;
     }
