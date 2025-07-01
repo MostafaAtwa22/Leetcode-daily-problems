5021c241406163559947fe96da055b4d40477240
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-07-01 19:38:40 +0300

    Time: 0 ms (100.00%) | Memory: 9.8 MB (77.70%) - LeetSync

--- 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
-    bool check (vector<int> & a, int start)
-    {
+    int minStartValue(vector<int>& a) {
+        int start = 0, mn = 0;
         for (auto i : a)
         {
             start += i;
-            if (start <= 0)
-                return false;
+            mn = min(start, mn);
         }
-        return true;
-    }
-    int minStartValue(vector<int>& a) {
-        int l = 1, r = 1e4, ans = -1, m; 
-        while (l <= r)
-        {
-            m = l + (r - l) / 2;
-            if (check (a, m))
-            {
-                r = m - 1;
-                ans = m;
-            }
-            else
-                l = m + 1;
-        }
-        return ans;
+        return mn < 0 ? abs(mn) + 1 : mn + 1;
     }
 };
