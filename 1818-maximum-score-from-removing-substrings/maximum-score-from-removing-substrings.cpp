685f9e2c502cddeeb7b5bf89b2bff173069ed99c
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-07-23 10:44:08 +0300

    Time: 36 ms (70.45%) | Memory: 26.2 MB (70.02%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        string st;
         int ans = 0;
-        if (x > y) {
-            stack<char> stack;
-            for (auto i : s) {
-                if (!st.empty() && st.back() == 'a' && i == 'b')
-                    ans += x, st.pop_back();
-                else
-                    st.push_back(i);
-            }
-            for (auto i : st) {
-                if (!stack.empty() && stack.top() == 'b' && i == 'a')
-                    ans += y, stack.pop();
-                else
-                    stack.push(i);
-            }
-        } 
-        else {
-            stack<char> stack;
-            for (auto i : s) {
-                if (!st.empty() && st.back() == 'b' && i == 'a')
-                    ans += y, st.pop_back();
-                else
-                    st.push_back(i);
-            }
-            for (auto i : st) {
-                if (!stack.empty() && stack.top() == 'a' && i == 'b')
-                    ans += x, stack.pop();
-                else
-                    stack.push(i);
-            }
+        stack<char> stack;
+        string st = "";
+        char min = 'a', max = 'b';
+        int mnN = y , mxN = x;
+        if (x < y) {
+            min = 'b', max = 'a';
+            mnN = x, mxN = y;
+        }
+        for (auto i : s) {
+            if (!st.empty() && st.back() == min && i == max)
+                ans += mxN, st.pop_back();
+            else
+                st.push_back(i);
+        }
+        for (auto i : st) {
+            if (!stack.empty() && stack.top() == max && i == min)
+                ans += mnN, stack.pop();
+            else
+                stack.push(i);
         }
         return ans;
     }
