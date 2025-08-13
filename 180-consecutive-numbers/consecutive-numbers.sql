44c98175370f86f9259096657ea65ba98da81d3c
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-08-13 19:03:43 +0300

    Time: 2030 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,9 +1,9 @@
 /* Write your T-SQL query statement below */
 Select Distinct num as ConsecutiveNums
-From Logs l
-Where 
-    id <= (select max(id) - 2 as id from Logs)
-AND 
-    num = (Select num From Logs Where id = l.id + 1)
-AND 
-    num = (Select num From Logs Where id = l.id + 2)
+From (
+    Select num,
+        Lead(num) Over (ORder By Id) as prev,
+        lag(num) Over (ORder By Id) as next
+    From Logs
+) as t
+Where prev = next and prev = num
