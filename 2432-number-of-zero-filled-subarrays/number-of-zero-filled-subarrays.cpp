ec2df70cfd5584e80567fd1f7af0668e95ae2e0e
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-08-19 03:30:38 +0300

    Time: 7 ms (12.52%) | Memory: 111.2 MB (95.09%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -4,14 +4,9 @@ public:
         long long ans = 0;
         long long cnt = 0;
         for (int i = 0; i < a.size(); i++) {
-            if (a[i] == 0)
-                cnt++;
-            else {
-                ans += (cnt * (cnt + 1)) / 2;
-                cnt = 0;
-            }
+            cnt = (a[i]) ? 0 : cnt + 1;
+            ans += cnt;
         }
-        ans += (cnt * (cnt + 1)) / 2;
         return ans;
     }
 };
