5bb32ac012557c15accf5c2a8817229148b82292
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-09-30 03:46:54 +0300

    Time: 57 ms (68.53%) | Memory: 22.3 MB (88.18%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
@@ -2,23 +2,11 @@ class Solution {
 public:
     int triangularSum(vector<int>& a) {
         int n = a.size();
-        deque<int> dq;
-        for (auto i : a)
-            dq.push_back(i);
-        int j = 1;
-        while (dq.size() > 1) {
-            int x = (dq[1] + dq[0]) % 10;
-            if (dq[0] + dq[1] < 10)
-                x = dq[1] + dq[0];
-            dq.push_back(x);
-            dq.pop_front();
-            j++;
-            if (j == n) {
-                n--;
-                dq.pop_front();
-                j = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                a[j] = (a[j] + a[j + 1]) % 10;
             }
-        }   
-        return dq[0];
+        }
+        return a[0];
     }
 };
