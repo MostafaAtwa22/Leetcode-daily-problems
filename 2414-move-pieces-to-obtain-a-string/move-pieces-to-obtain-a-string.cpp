cd4ecb845922c010d153448fc047fbc20932079c
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-10-02 18:38:13 +0300

    Time: 1656 ms (5.13%) | Memory: 21.8 MB (73.76%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
@@ -3,11 +3,13 @@ public:
     bool canChange(string start, string target) {
         int n = start.size();
         int i = 0, j = 0;
-        while (i < n && j < n) {
+        while (i < n || j < n) {
             while (i < n && start[i] == '_')
                 i++;
             while (j < n && target[j] == '_')
                 j++;
+            if (i == n || j == n)
+                return i == j;
             if (start[i] != target[j])
                 return false;
             else if (start[i] == 'L' && j > i)
@@ -17,10 +19,6 @@ public:
             cout << i << ' ' << j << '\n';
             i++, j++;
         }
-        while (i < n && start[i] == '_')
-            i++;
-        while (j < n && target[j] == '_')
-            j++;
         return i == j;
     }
 };
