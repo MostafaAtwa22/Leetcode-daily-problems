e8223545079aee14ac67e5aa93f5b32585807859
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-10-10 09:43:48 +0300

    Time: 1 ms (9.61%) | Memory: 9.6 MB (59.92%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
@@ -3,12 +3,9 @@ public:
     int distinctAverages(vector<int>& a) {
         sort(a.begin(), a.end());
         int l = 0, r = a.size() - 1;
-        set<double> se;
+        unordered_map<int, int> mp;
         while (l < r)
-        {
-            se.insert((a[l] * 1.0 + a[r] * 1.0) / 2.0);
-            l++, r--;
-        }
-        return se.size();
+            mp[a[l++] + a[r--]]++;
+        return mp.size();
     }
 };
