f5acffdd8fe74ff511a9fd32bd1ab90f5156f060
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-10-17 12:24:36 +0300

    Time: 35 ms (31.23%) | Memory: 64.9 MB (89.64%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& a) {
+        sort(a.begin(), a.end());
+        for (int i = 1; i < a.size(); i++) {
+            if (a[i] == a[i - 1])
+                return a[i];
+        }
+        return a[0];
+    }
+};
