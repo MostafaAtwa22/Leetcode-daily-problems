aeaf8166008e4478802b03bb6386b2dd5265be9a
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-11-26 09:17:36 +0200

    Time: 0 ms (100.00%) | Memory: 7.7 MB (96.87%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int sum(int n) {
         int ans = 0;
+        int x;
         while (n) {
-            ans += pow(n % 10, 2);
+            x = n % 10;
+            ans += x * x;
             n /= 10;
         }
         return ans;
     }
     bool isHappy(int n) {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100; i++) {
             if (n == 1)
                 return true;
             n = sum(n);
