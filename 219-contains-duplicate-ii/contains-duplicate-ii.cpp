eb483fbe031f5496bd6daf36adb238ec6af68533
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-11-26 09:24:56 +0200

    Time: 55 ms (99.32%) | Memory: 98.9 MB (15.93%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& a, int k) {
-        unordered_map<int, vector<int>> mp;
+        unordered_map<int, int> mp;
         int n = a.size();
         for (int i = 0; i < n; i++) {
-            if (mp.find(a[i]) != mp.end()) {
-                for (auto j : mp[a[i]]) {
-                    if (abs(i - j) <= k)
-                        return true;
-                }
-            }
-            mp[a[i]].push_back(i);
+            if (mp.find(a[i]) != mp.end() && (i - mp[a[i]]) <= k)
+                return true;
+            mp[a[i]] = i;
         }
         return false;
     }
