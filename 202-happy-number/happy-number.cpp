fbece66568755caed3445131e6cfebf2c4b41a28
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-11-26 09:16:16 +0200

    Time: 7 ms (3.95%) | Memory: 7.8 MB (85.89%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,21 +1,18 @@
-int convert(int n) {
-    int output = 0;
-    
-    while (n > 0) {
-        int digit = n % 10;
-        output += digit * digit;
-        n = n / 10;
-    }
-    
-    return output;
-}
 class Solution {
 public:
+    int sum(int n) {
+        int ans = 0;
+        while (n) {
+            ans += pow(n % 10, 2);
+            n /= 10;
+        }
+        return ans;
+    }
     bool isHappy(int n) {
         for (int i = 0; i < 1000; i++) {
             if (n == 1)
                 return true;
-            n = convert(n);
+            n = sum(n);
         }
         return false;
     }
