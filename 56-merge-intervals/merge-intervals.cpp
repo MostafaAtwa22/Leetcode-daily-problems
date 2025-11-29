ba87f3c427f3b219ac08659514f5a4bc878a7b4d
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-11-29 10:06:45 +0200

    Time: 3 ms (89.83%) | Memory: 23.7 MB (93.69%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& a) {
+        sort(a.begin(), a.end());
+        int n = a.size();
+        int start = a[0][0], end = a[0][1];
+        vector<vector<int>> ans;
+        for (int i = 1; i < n; i++) {
+            if (a[i][0] >= start && a[i][0] <= end) {
+                if (end < a[i][1])
+                    end = a[i][1];
+            }
+            else {
+                ans.push_back({start, end});
+                start = a[i][0], end = a[i][1];
+            }
+        }
+        ans.push_back({start, end});
+        return ans;
+    }
+};
