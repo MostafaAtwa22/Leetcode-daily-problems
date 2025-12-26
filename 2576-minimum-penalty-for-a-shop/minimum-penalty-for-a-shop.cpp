cb70e929c4f2457c408cbe2a0157747c1f17eb62
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2025-12-26 06:33:43 +0200

    Time: 0 ms (100.00%) | Memory: 13.2 MB (98.36%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -2,24 +2,17 @@ class Solution {
 public:
     int bestClosingTime(string s) {
         int n = s.size();
-        int mx = INT_MAX;
+        int mx = 0;
         int ans = 0;
-        vector<int> Y(n + 1, 0), N(n + 1, 0);
+        int p = 0;
         for (int i = 0; i < n; i++) {
-            if (s[i] == 'Y')
-                Y[i + 1] = 1;
-            else
-                N[i + 1] = 1;
-            Y[i + 1] += Y[i];
-            N[i + 1] += N[i];
-        }
-        for (int i = 0; i <= n; i++) {
-            int x = Y[n] - Y[i] + (N[i] - N[0]);
-            if (mx > x) {
-                mx = x;
-                ans = i;
+            p += (s[i] == 'Y') ? -1 : 1;
+            if (mx > p) {
+                mx = p;
+                ans = i + 1;
             }
         }
+
         return ans;
     }
 };
