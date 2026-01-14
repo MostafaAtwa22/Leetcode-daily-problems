8c7b257b458031c1de73df1cef9ce8071bbfaa4d
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-01-15 00:15:10 +0200

    Time: 35 ms (90.65%) | Memory: 62.5 MB (68.33%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    unordered_map<int, bool> mp;
-    void fun(int n, int k, int i, vector<int> a, vector<vector<int>> & ans) {
-        if (i == k) {
+    void fun(int n, int k, int i, vector<int>& a, vector<vector<int>> & ans) {
+        if (a.size() == k) {
             ans.push_back(a);
             return;
         }
-        for (int j = 1; j <= n; j++) {
-            if (mp[j])
-                return;
+        for (int j = i; j <= n; j++) {
             // do 
             a.push_back(j);
-            mp[j] = true;
             // rec
-            fun(n, k, i + 1, a, ans);
+            fun(n, k, j + 1, a, ans);
             // undo
             a.pop_back();
-            mp[j] = false;
         }
     }
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
-        fun(n, k, 0, {}, ans);
+        vector<int> a;
+        fun(n, k, 1, a, ans);
         return ans;
     }
 };
