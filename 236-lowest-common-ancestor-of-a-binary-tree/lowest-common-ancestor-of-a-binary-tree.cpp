f3719e98d7ad327a754f83d133c2fff04a79e993
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-01-21 18:19:36 +0200

    Time: 12 ms (65.69%) | Memory: 17.3 MB (65.49%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,28 +9,14 @@
  */
 class Solution {
 public:
-    void check (TreeNode * root, TreeNode * p, TreeNode * q, int &cnt) {
-        if (!root || cnt == 0)
-            return;
-        if (root == p || root == q)
-            cnt--;
-        check(root->left, p, q, cnt);
-        check(root->right, p, q, cnt);
-    }
-    TreeNode * ans;
-    void pre(TreeNode * root, TreeNode * p, TreeNode * q) {
-        if (!root)
-            return;
-        int cnt = 2;
-        check(root, p, q, cnt);
-        if (cnt == 0)
-            ans = root;
-        pre(root->left, p, q);
-        pre(root->right, p, q);
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        ans = root;
-        pre(root, p, q);
-        return ans;
+        if (!root || root == p || root == q)
+            return root;
+        TreeNode * l = lowestCommonAncestor(root->left, p, q);
+        TreeNode * r = lowestCommonAncestor(root->right, p, q);
+
+        if (l && r)
+            return root;
+        return l ? l : r;
     }
 };
