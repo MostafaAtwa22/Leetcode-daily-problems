483b8acf871de7f7a4d2975733f549d80fbd53b1
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-02-11 13:18:37 +0200

    Time: 1163 ms (5.01%) | Memory: 19.3 MB (6.38%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool vis[202][202];
+    int n, m;
+
+
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+
+    bool valid (int i, int j)
+    {
+        return i >= 0 && i < n && j >= 0 && j < m;
+    }
+
+    void dfs(vector<vector<char>>& a, int curx, int cury, bool& flg)
+    {
+        vis[curx][cury] = 1;
+        cout << curx << ' ' << cury << '\n';
+        if (curx == 0 || curx == n - 1 || cury == 0 || cury == m - 1) {
+            flg = false;
+            return;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = curx + dx[i];
+            int ny = cury + dy[i];
+            if (valid(nx, ny) && a[nx][ny] == 'O' && !vis[nx][ny])
+            {
+                dfs(a, nx, ny, flg);
+            }
+        }
+    }
+    void dfs(vector<vector<char>>& a, int curx, int cury)
+    {
+        a[curx][cury] = 'X';
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = curx + dx[i];
+            int ny = cury + dy[i];
+            if (valid(nx, ny) && a[nx][ny] == 'O' && vis[nx][ny])
+            {
+                dfs(a, nx, ny);
+            }
+        }
+    }
+    void solve(vector<vector<char>>& a) {
+        n = a.size(), m = a[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] == 'O' && !vis[i][j]) {
+                    bool flg = true;
+                    dfs(a, i, j, flg);
+                    cout << i << " " << j << ' ';
+                    (flg == true) ? cout << "True\n" : cout << "False" << '\n';
+                    if (flg == true)
+                        dfs(a, i, j);
+                    else
+                        fill(&vis[0][0], &vis[0][0] + 202*202, false);
+                }
+            }
+        }
+    }
+};
