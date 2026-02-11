5dbfb7a0c322bc05f0d00c06ebcf97e6be050b29
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-02-11 13:29:30 +0200

    Time: 0 ms (100.00%) | Memory: 14 MB (75.06%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -3,7 +3,6 @@ public:
     bool vis[202][202];
     int n, m;
 
-
     int dx[4] = {0, 0, 1, -1};
     int dy[4] = {1, -1, 0, 0};
 
@@ -12,31 +11,14 @@ public:
         return i >= 0 && i < n && j >= 0 && j < m;
     }
 
-    void dfs(vector<vector<char>>& a, int curx, int cury, bool& flg)
+    void dfs(vector<vector<char>>& a, int curx, int cury)
     {
         vis[curx][cury] = 1;
-        if (curx == 0 || curx == n - 1 || cury == 0 || cury == m - 1) {
-            flg = false;
-            return;
-        }
         for (int i = 0; i < 4; i++)
         {
             int nx = curx + dx[i];
             int ny = cury + dy[i];
             if (valid(nx, ny) && a[nx][ny] == 'O' && !vis[nx][ny])
-            {
-                dfs(a, nx, ny, flg);
-            }
-        }
-    }
-    void dfs(vector<vector<char>>& a, int curx, int cury)
-    {
-        a[curx][cury] = 'X';
-        for (int i = 0; i < 4; i++)
-        {
-            int nx = curx + dx[i];
-            int ny = cury + dy[i];
-            if (valid(nx, ny) && a[nx][ny] == 'O' && vis[nx][ny])
             {
                 dfs(a, nx, ny);
             }
@@ -45,16 +27,21 @@ public:
     void solve(vector<vector<char>>& a) {
         n = a.size(), m = a[0].size();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (a[i][j] == 'O' && !vis[i][j]) {
-                    bool flg = true;
-                    dfs(a, i, j, flg);
-                    if (flg == true)
-                        dfs(a, i, j);
-                    else
-                        fill(&vis[0][0], &vis[0][0] + 202*202, false);
-                }
-            }
+            if (a[i][0] == 'O' && !vis[i][0])
+                dfs(a, i, 0);
+            if (a[i][m - 1] == 'O' && !vis[i][m - 1])
+                dfs(a, i, m - 1);
         }
+        for (int j = 0; j < m; j++) {
+            if (a[0][j] == 'O' && !vis[0][j])
+                dfs(a, 0, j);
+            if (a[n - 1][j] == 'O' && !vis[n - 1][j])
+                dfs(a, n - 1, j);
+        }
+
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < m; j++)
+                if (!vis[i][j] && a[i][j] == 'O')
+                    a[i][j] = 'X';
     }
 };
