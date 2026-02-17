31e2f751181f62bd54f1d670b54c8ed966403424
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-02-17 23:02:23 +0200

    Time: 32 ms (11.09%) | Memory: 23.6 MB (8.04%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.cpp
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -1,23 +1,64 @@
+struct PairHash {
+    size_t operator()(const pair<int,int>& p) const {
+        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+    }
+};
+
 class Solution {
 public:
-    int snakesAndLadders(vector<vector<int>>& board) {
-        int n = board.size();
-        vector<int> min_rolls(n * n + 1, -1);
-        queue<int> q;
-        min_rolls[1] = 0;
-        q.push(1);
+    int snakesAndLadders(vector<vector<int>>& a) {
+        int n = a.size(), m = a[0].size();
+        unordered_map<int, pair<int, int>> mp;
+        unordered_map<pair<int, int>, int, PairHash> val;
+
+        int num = 1;
+        bool flg = true;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (flg) {
+                for (int j = 0; j < m; j++) {
+                    mp[num] = {i, j};
+                    if (a[i][j] != -1)
+                        val[{i, j}] = a[i][j];
+                    a[i][j] = num++;
+                }
+            } else {
+                for (int j = m - 1; j >= 0; j--) {
+                    mp[num] = {i, j};
+                    if (a[i][j] != -1)
+                        val[{i, j}] = a[i][j];
+                    a[i][j] = num++;
+                }
+            }
+            flg = !flg;
+        }
+
+        queue<pair<int,int>> q; // {value, distance}
+        q.push({1, 0});
+
+        unordered_map<int, bool> vis;
+        vis[1] = true;
+
         while (!q.empty()) {
-            int x = q.front();
+            auto [value, dis] = q.front();
             q.pop();
-            for (int i = 1; i <= 6 && x + i <= n * n; i++) {
-                int t = x + i, row = (t - 1) / n, col = (t - 1) % n;
-                int v = board[n - 1 - row][row % 2 ? n - 1 - col : col];
-                int y = v > 0 ? v : t; // follow ladder / snake if present
-                if (y == n * n)
-                    return min_rolls[x] + 1;
-                if (min_rolls[y] == -1) {
-                    min_rolls[y] = min_rolls[x] + 1;
-                    q.push(y);
+
+            if (value == n * m)
+                return dis;
+
+            for (int i = 1; i <= 6; i++) {
+                int nxt = value + i;
+                if (nxt > n * m) continue;
+
+                int r = mp[nxt].first;
+                int c = mp[nxt].second;
+
+                if (val.count({r, c}))
+                    nxt = val[{r, c}];
+
+                if (!vis[nxt]) {
+                    vis[nxt] = true;
+                    q.push({nxt, dis + 1});
                 }
             }
         }
