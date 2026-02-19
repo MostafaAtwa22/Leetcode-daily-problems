b552fcab82ae5557c911c21479f3a33427d185e0
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-02-19 14:02:16 +0200

    Time: 0 ms (100.00%) | Memory: 13.3 MB (24.26%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -3,26 +3,16 @@ public:
     int countBinarySubstrings(string s) {
         int n = s.size();
         int cnt = 0;
-        int z = 0, o = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '1') {
-                o++;
-            }
-            else if (s[i] == '0') {
-                z++;
-            }
-            if (i == n - 1 && z && o) 
-                cnt += min(o, z);
-            else if (i < n - 1 && z && o && s[i] != s[i + 1]) {
-                cnt += min(o, z);
-                if (s[i + 1] == '0')
-                    z = 0;
-                else
-                    o = 0;
+        for (int i = 1; i < n; ++i) {
+            if (s[i] != s[i - 1]) {
+                int l = i - 1, r = i;
+                char left = s[l], right = s[r];
+                while (l >= 0 && r < n && s[l] == left && s[r] == right) {
+                    cnt++;
+                    l--, r++;
+                }
             }
         }
-
         return cnt;
     }
 };
