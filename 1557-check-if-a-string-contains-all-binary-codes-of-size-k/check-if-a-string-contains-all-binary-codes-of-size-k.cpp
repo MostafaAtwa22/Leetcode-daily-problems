20046f336ac9ee5977613ac0361aed14926df061
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-02-23 14:25:05 +0200

    Time: 414 ms (57.79%) | Memory: 119.1 MB (59.96%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,32 +1,10 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        unordered_map<string, bool> mp;
-        int r = k, l = 0;
-        while (r <= s.size()) {
-            string ss = "";
-            for (int j = l; j < r; j++) {
-                ss += s[j];
-            }
-            mp[ss] = true;
-            l++, r++;
-        }
-        
-
-        string str = "";
-        for (int i = 0; i < k; i++)
-            str += '0';
-        for (int i = 0; i < (1 << k); i++) {
-            string copy = str;
-            int msk = i;
-            for (int j = 0; j < k; j++) {
-                if ((msk & 1))
-                    copy[j] = '1';
-                msk >>= 1;
-            }
-            if (!mp[copy])
-                return false;
-        }
-        return true;
+        int n = s.size();
+        unordered_set<string> se;
+        for (int i = 0; i <= n - k; i++)
+            se.insert(s.substr(i, k));
+        return (int)se.size() == (1 << k);
     }
 };
