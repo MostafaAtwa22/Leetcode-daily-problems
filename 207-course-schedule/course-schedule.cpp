38baf04512a37e9edb5d403b0d6fed6db93dbee5
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-03-03 23:48:16 +0200

    Time: 0 ms (100.00%) | Memory: 18.6 MB (96.04%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -8,13 +8,13 @@ public:
             g[a[i][0]].push_back(a[i][1]);
             inDeg[a[i][1]]++;
         }
-        queue<int> q;
+        priority_queue<int, vector<int>, greater<int>> q; 
         for (int i = 0; i < n; i++)
             if (!inDeg[i])
                 q.push(i);
         int cnt = 0;
         while (!q.empty()) {
-            int u = q.front();
+            int u = q.top();
             q.pop();
             cnt++;
             for (auto v : g[u]) {
