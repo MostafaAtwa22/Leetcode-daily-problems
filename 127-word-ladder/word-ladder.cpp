05e37485dc7f723bb4eb15a5d6e4d6663f2f13f1
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-03-06 23:35:00 +0200

    Time: 1739 ms (5.00%) | Memory: 42.6 MB (8.33%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,32 +1,47 @@
 class Solution {
-public:
-    int ladderLength(string from, string to, vector<string>& a) {
-        unordered_set<string> g(a.begin(), a.end());
-
-        if (!g.count(to))
-            return 0;
-        queue<pair<string, int>> q;
-        q.push({from, 1});
+public: 
+    bool comp(string s1, string s2) {
+        int cnt = 0;
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] != s2[i])
+                cnt++;
+        }
+        return cnt == 1;
+    }
+    int ladderLength(string begin, string end, vector<string>& a) {
+        unordered_map<string, vector<string>> g;
         unordered_map<string, bool> vis;
-        vis[from] = 1;
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            if (comp(begin, a[i])) {
+                g[begin].push_back(a[i]);
+                g[a[i]].push_back(begin);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (comp(a[i], a[j])) {
+                    g[a[i]].push_back(a[j]);
+                    g[a[j]].push_back(a[i]);
+                }
+            }
+        }
+        
+        queue<pair<string, int>> q;
+        q.push({begin, 1});
+        vis[begin] = 1;
         while (!q.empty()) {
             string u = q.front().first;
-            int dist = q.front().second;
+            int d = q.front().second;
             q.pop();
-            if (u == to)
-                return dist;
-            for (int i = 0; i < u.size(); i++) {
-                char org = u[i];
-                for (char c = 'a'; c <= 'z'; c++) {
-                    if (org == c)
-                        continue;
-                    u[i] = c;
-                    if (!vis[u] && g.count(u)) {
-                        vis[u] = 1;
-                        q.push({u, dist + 1});
-                    }
+            if (u == end)
+                return d;
+            for (auto v : g[u]) {
+                if (!vis[v]) {
+                    vis[v] = true;
+                    q.push({v, d + 1});
                 }
-                u[i] = org;
             }
         }
         return 0;
