1a1bfebf0c71f981cc3652ebe0f9cc866564ab4d
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-03-11 12:40:55 +0200

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.24%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0)
+            return 1;
+        int ans = 0;
+        int i = 0;
+        while (n) {
+            if (!(n & 1)) 
+                ans |= (1 << i);
+            n >>= 1;
+            i++;
+        }
+        return ans;
+    }
+};
