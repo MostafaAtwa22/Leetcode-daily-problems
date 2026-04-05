b30e9ce734482de40c9a9100164178f200f616e1
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-04-05 09:16:48 +0200

    Time: 0 ms (100.00%) | Memory: 10.4 MB (47.26%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool judgeCircle(string s) {
+        int n = s.size();
+        int r = 0, c = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'U')
+                r++;
+            else if (s[i] == 'D')
+                r--;
+            else if (s[i] == 'L')
+                c--;
+            else
+                c++;
+        }
+        return r == 0 && c == 0;
+    }
+};
