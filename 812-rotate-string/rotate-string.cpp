cd6a7da49a74ba0743a038c2b88a69baa78b3c6e
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-05-03 13:41:19 +0300

    Time: 0 ms (100.00%) | Memory: 9 MB (12.66%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    bool check (int n, deque<char> & q, string & g) {
-        for (int i = 0; i < n; i++)
-            if (q[i] != g[i])
-                return false;
-        return true;
-    }
     bool rotateString(string s, string g) {
         int n = s.size();
         int m = g.size();
         if (n != m)
             return false;
-        deque<char> q;
-        for (auto i : s)
-            q.push_back(i);
         for (int i = 0; i < n; i++) {
-            if (check(n, q, g))
+            if (g == s)
                 return true;
-            q.push_back(q.front());
-            q.pop_front();
+            s = s.substr(1) + s.substr(0, 1);
         }
         return false;
     }
