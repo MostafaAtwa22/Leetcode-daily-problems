f12ad782f20aff9bc06c7f06ea998e3957915693
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-05-10 09:37:13 +0300

    Time: 5 ms (99.07%) | Memory: 68.4 MB (69.25%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int sol(vector<int> & a, int n, int t, vector<int> & dp) {
-        if (n <= 0)
-            return 0;
-        if (dp[n] != -1)
-            return dp[n];
-        dp[n] = INT_MIN;
-        for (int i = n - 1; i >= 0; i--) {
-            if (a[n] - a[i] >= -t && a[n] - a[i] <= t)
-                dp[n] = max(dp[n], sol(a, i, t, dp) + 1);
-        }
-        return dp[n];
-    }
     int maximumJumps(vector<int>& a, int t) {
         int n = a.size();
         vector<int> dp(n, -1);
-        int res = sol(a, n - 1, t, dp);
-        return res < 0 ? -1 : res;
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == -1)    
+                continue;
+            for (int j = i + 1; j < n; j++) {
+                if (abs(a[j] - a[i]) <= t)
+                    dp[j] = max(dp[j], dp[i] + 1);
+            }
+        }
+       
+        return dp[n - 1];
     }
 };
