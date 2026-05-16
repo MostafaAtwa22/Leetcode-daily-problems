f2599ee4cbcda9ec53fdb4c96bd1d7ae95ce6e4a
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-05-16 08:25:04 +0300

    Time: 0 ms (100.00%) | Memory: 16.2 MB (27.34%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    bool checkright(vector<int> & a, int r, int n, int val) {
-        for (int i = r; i < n; i++)
-            if (a[i] < val)
-                return true;
-        return false;
-    }
     int findMin(vector<int>& a) {
         int n = a.size();
         int l = 0, r = n - 1, m;
         while (l < r) {
             m = l + (r - l) / 2;
-            if (checkright(a, m + 1, n, a[m]))  
+            if (a[m] > a[r])  
                 l = m + 1;
-            else
+            else if (a[m] < a[r])
                 r = m;
+            else
+                r--;
         }
         return a[r];
     }
