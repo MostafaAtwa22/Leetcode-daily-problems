84735953e82c5db7426ab7c5dd35230671f411e5
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-05-16 08:19:11 +0300

    Time: 7 ms (0.59%) | Memory: 17.3 MB (27.34%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int findMin(vector<int>& a) {
-        return *min_element(a.begin(), a.end());
+        int n = a.size();
+        vector<int> arr;
+        unordered_map<int, bool> mp;
+        for (auto i : a) {
+            if (!mp[i]) 
+                arr.push_back(i);
+            mp[i] = true;
+        }
+        int l = 0, r = arr.size() - 1, m;
+        while (l < r) {
+            m = l + (r - l) / 2;
+            if (arr[m] > arr[r])
+                l = m + 1;
+            else
+                r = m;
+        }
+        return arr[r];
     }
 };
