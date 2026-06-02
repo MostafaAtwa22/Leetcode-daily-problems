c809e064f3748ae23d09cd6d9218bbaa4b3fbb55
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-06-02 08:02:31 +0300

    Time: 19 ms (12.24%) | Memory: 92.1 MB (12.24%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& a, vector<int>& b, vector<int>& c, vector<int>& d) {
+    int earliestFinishTime(vector<int>& lt, vector<int>& ld,
+                           vector<int>& wt, vector<int>& wd) {
+
         int ans = INT_MAX;
-        // take land first
 
-        int n = a.size();
-        int minEnd = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            minEnd = min(minEnd, a[i] + b[i]);
-        }
-        int m = c.size();
+        for (int i = 0; i < lt.size(); i++) {
+            for (int j = 0; j < wt.size(); j++) {
 
-        for (int i = 0; i < m; i++) {
-            ans = min(ans, d[i] + max(minEnd, c[i]));
-        }
+                int landFirst =
+                    max(lt[i] + ld[i], wt[j]) + wd[j];
 
-        // take water first;
-        minEnd = INT_MAX;
-        for (int i = 0; i < m; i++) {
-            minEnd = min(minEnd, c[i] + d[i]);
-        }
+                int waterFirst =
+                    max(wt[j] + wd[j], lt[i]) + ld[i];
 
-        for (int i = 0; i < n; i++) {
-            ans = min(ans, b[i] + max(minEnd, a[i]));
+                ans = min({ans, landFirst, waterFirst});
+            }
         }
 
         return ans;
