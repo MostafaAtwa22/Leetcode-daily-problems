178024c8c820431ea404d77da8f17de7a85b26f2
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-06-08 07:13:35 +0300

    Time: 4 ms (81.51%) | Memory: 127.6 MB (98.06%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& a, int p) {
         int n = a.size();
-        vector<pair<int, pair<int, int>>> arr(n, {0, {0, 0}});
+        vector<int> arr(n);
 
-        for (int i = 0; i < n; i++) {
+        int k = 0;
+        for (int i = 0; i < n; i++) 
+            if (a[i] < p)
+                arr[k++] = a[i];
+
+        for (int i = 0; i < n; i++) 
+            if (a[i] == p)
+                arr[k++] = a[i];
+
+        for (int i = 0; i < n; i++) 
             if (a[i] > p)
-                arr[i].first = 2;
-            else if (a[i] < p)
-                arr[i].first = 0;
-            else
-                arr[i].first = 1;
-            arr[i].second.first = i;
-            arr[i].second.second = a[i];
-        }
-
-        sort(arr.begin(), arr.end());
-        for (int i = 0; i < n; i++)
-            a[i] = arr[i].second.second;
-        return a;
+                arr[k++] = a[i];
+        return arr;
     }
 };
