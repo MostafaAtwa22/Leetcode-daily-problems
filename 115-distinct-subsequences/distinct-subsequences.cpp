d43b45c813268c4e69f03601a9d88cb178bec2b4
Mostafa Atwa <149294411+MostafaAtwa22@users.noreply.github.com>
2026-09-06 06:34:38 +0300

    Time: 30 ms (58.87%) | Memory: 13 MB (80.54%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dp[1001][1001];
+
+    int sol(int i, int j, string& s, string& t) {
+        if (j == t.size())
+            return 1;
+
+        if (i == s.size())
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int ans = 0;
+
+        ans = sol(i + 1, j, s, t);
+
+        if (s[i] == t[j]) 
+            ans += sol(i + 1, j + 1, s, t);
+
+        return dp[i][j] = ans;
+    }
+
+    int numDistinct(string s, string t) {
+        memset(dp, -1, sizeof(dp));
+
+        return sol(0, 0, s, t);
+    }
+};
